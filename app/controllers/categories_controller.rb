d623c71a2a852fe0ffb89a90b82cf18ccffaee02
Everton Silva <thonns@gmail.com>
2017-08-28 19:53:43 -0300

    feature: add controller methods to handle commom requests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,31 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
+
   def index
+    @cats = Category.all
   end
 
   def show
   end
 
+  def new
+    @cat = Category.new
+  end
+
   def edit
   end
 
-  def new
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def find_category
+    @cat = Category.find(params[:id])
   end
 end
