b6fb82ac5e69b5a2cafd54177df1803f9d97c694
Everton Silva <thonns@gmail.com>
2017-08-28 19:49:05 -0300

    chore: setup files to CRUD operations for Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827235031) do
+ActiveRecord::Schema.define(version: 20170828223429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "drinks", force: :cascade do |t|
     t.string "name"
