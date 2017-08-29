3a2b6ce5b8067ce6a9fd418ac6840b5fc181dd50
Everton Silva <thonns@gmail.com>
2017-08-29 20:58:02 -0300

    feature: validate expiration date before save a drink

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -9,4 +9,14 @@
 class Drink < ApplicationRecord
   belongs_to :category
   validates :name, :expiration_date, :manufacture_date, :alcoholic_content, presence: true
+  validate :check_experiation_date
+
+  private
+    def check_experiation_date
+      max_period = self.category.max_permanency_period
+      if self.expiration_date > (self.manufacture_date.to_datetime >> max_period)
+        self.errors[:expiration_date] << I18n.t("Date can be above #{max_period} months from #{manufacture_date.to_s}")
+      end
+    end
+
 end
