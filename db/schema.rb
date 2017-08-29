2fbb91a2277def411ade532072fe502803965033
Everton Silva <thonns@gmail.com>
2017-08-29 11:42:46 -0300

    feature: add max permanency date field to Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829002042) do
+ActiveRecord::Schema.define(version: 20170829013154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170829002042) do
     t.string "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "max_permanency_date"
   end
 
   create_table "drinks", force: :cascade do |t|
