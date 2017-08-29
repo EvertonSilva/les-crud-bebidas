5766807e58c100509e8eff722c78eee338c7f3ec
Talles Costa <chavinho_costa@hotmail.com>
2017-08-29 11:16:24 -0300

    feature: generate routes to CRUD and create operation done

---
+++ app/controllers/manufacturers_controller.rb
@@ -0,0 +1,40 @@
+class ManufacturersController < ApplicationController
+  before_action :find_manufac, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  	@manufac = Manufacturer.new
+  end
+
+  def edit
+  end
+
+  def create
+  	@manufac = Manufacturer.new(manufac_params)
+  	if @manufac.save
+  		redirect_to @manufac
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def find_manufac
+  	@manufac = Manufacturer.find(params[:id])
+  end
+
+  def manufac_params
+  	params.require(:manufacturer).permit(:name, :cnpj)
+  end
+end
