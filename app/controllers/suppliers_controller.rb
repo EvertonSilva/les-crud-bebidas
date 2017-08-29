782eaec7f4195dfd620287a345c35964881b8fe0
Talles Costa <chavinho_costa@hotmail.com>
2017-08-29 13:04:50 -0300

    feature: supplier create operation done

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,15 +1,36 @@
 class SuppliersController < ApplicationController
+  before_action :find_supplier, only: [:show, :edit, :update, :destroy]
+
   def index
-    @supplers = Supplier.all
+    @suppliers = Supplier.all
   end
 
   def show
   end
 
   def new
-    @suppler = Supplier.new
+    @supplier = Supplier.new
   end
 
   def edit
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+    if @supplier.save
+      redirect_to @supplier
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def find_supplier
+    @supplier = Supplier.find(params[:id])
+  end
+
+  def supplier_params
+    params.require(:supplier).permit(:name, :registry, :phone, :email)
   end
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,11 @@
+# Class to persist a Supplier object
+#
+# table name: suppliers
+# fields:type
+#   name:String
+#   registry:String
+#   phone:String
+#   email:String
 class Supplier < ApplicationRecord
+	validates :name, :registry, :phone, :email, presence: true
 end
