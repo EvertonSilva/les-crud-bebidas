c0be0906c0000015f8f69689420b3c3fc5342fbe
Talles Costa <chavinho_costa@hotmail.com>
2017-08-29 13:26:44 -0300

    refactor: migration manufacturer rename column cnpj to email

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -5,5 +5,5 @@
 #   name:String
 #   cnpj:String
 class Manufacturer < ApplicationRecord
-	validates :name, :cnpj, :phone, :email, presence: true
+	validates :name, :registry, :phone, :email, presence: true
 end
